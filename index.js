3d42bc5195c45ea0d5ccec78f09f16ab9ce920cc
monicaiyb <monicaiyb@gmail.com>
2020-10-28 18:31:53 +0300

    Have added mongoose and pug

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-//I am importing express into my project
+//I am importing moduless into my project
 const express=require("express");
-
-
-// import the bodyparser middleware into my project
 const bodyParser= require('body-parser');
+const path=require('path');
+const mongoose=require("mongoose")
+
 
 // instatiateing express function in our file
 const app =express();
+
+// mongoose.connect("mongodb://localhost:27017/node-demo",{
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+//   },
+//   function(err) {
+//     if (err) throw err;
+//     console.log("Successfully connected");
+//   }
+// );
+
+
+app.set("view engine","pug");
+app.set("views", path.join(__dirname,"view"));
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -16,12 +30,15 @@ app.use(express.static('public'));
 
 
 app.use('/public', express.static(__dirname + '/public'));
+
 //Sending my homepage to the browser
 app.get("/",(req,res)=>{
     res.sendFile(__dirname +"/index.html");
     console.log("Hello welcome my Ufarm project");
 })
-
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/customer_login.html")
+})
 
 
  
